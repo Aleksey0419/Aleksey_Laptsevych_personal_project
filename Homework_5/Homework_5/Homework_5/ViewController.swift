be0b8c6f2a4ab1de790769da5988c3a9e5692c80
Aleksey <>
2021-11-11 20:35:06 +0200

    Initial commit

---
+++ Homework_5/Homework_5/Homework_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_5
+//
+//  Created by Aleksey on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
